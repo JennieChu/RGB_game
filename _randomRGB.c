a9b270b33391115545d267f265fbd8b028daa21e
Jennie Chu <jenniechu92@gmail.com>
2016-11-06 05:35:42 +0000

    created swatchGenerator: prints out same color swatch

--- _randomRGB.c
+++ _randomRGB.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * main - prints a RGB value in hex
  * Return: none;
@@ -12,22 +12,30 @@
  * Return: value
  */
 
-char *_rgbGenerator();
+unsigned char *_rgbGenerator(unsigned char *c);
+unsigned char *_swatchGenerator(unsigned char *color, unsigned char *swatch);
 
 int main(void)
 {
-	char s[10];
-	char *color;
+	unsigned char c[10], s1[10], s2[10], s3[10];
+	unsigned char *color, *swatch1, *swatch2, *swatch3;
 
 	srand(time(NULL));
-	color = _rgbGenerator(s);
-	printf("%s\n", color);
+	color = _rgbGenerator(c);
+	swatch1 = _swatchGenerator(color, s1);
+	printf("%s\n%s\n", color, swatch1);
 	return (0);
 }
 
-char *_rgbGenerator(char *s)
+unsigned char *_rgbGenerator(unsigned char *c)
 {
-	sprintf(s, "%02x%02x%02x", rand() % 255,
+	sprintf(c, "%02x%02x%02x", rand() % 255,
 		rand() % 255, rand() % 255);
-	return (s);
+	return (c);
+}
+
+unsigned char *_swatchGenerator(unsigned char *color, unsigned char *swatch)
+{
+	strcpy(swatch, color);
+	return (swatch);
 }

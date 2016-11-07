016cbd51a7bf6f12b09fd19de9be77caca90f33c
Jennie Chu <jenniechu92@gmail.com>
2016-11-07 03:55:29 +0000

    created pointers for each RGB value

--- _randomRGB.c
+++ _randomRGB.c
@@ -12,30 +12,41 @@
  * Return: value
  */
 
-unsigned char *_rgbGenerator(unsigned char *c);
+
+unsigned char *_rgbGenerator(unsigned char *c, int *r, int *g, int *b);
 unsigned char *_swatchGenerator(unsigned char *color, unsigned char *swatch);
+unsigned int *_randomVal(unsigned int *val);
 
 int main(void)
 {
 	unsigned char c[10], s1[10], s2[10], s3[10];
 	unsigned char *color, *swatch1, *swatch2, *swatch3;
+	int *r, *g, *b;
+	int r_val, g_val, b_val;
 
 	srand(time(NULL));
-	color = _rgbGenerator(c);
-	swatch1 = _swatchGenerator(color, s1);
+	r_val = rand() % 255;
+	g_val = rand() % 255;
+	b_val = rand() % 255;
+	r = &r_val;
+	g = &g_val;
+	b = &b_val;
+	color = _rgbGenerator(c, r, g, b);
+	swatch1 = _swatchGenerator(s1, r);
+	swatch2 = _swatchGenerator(s2, g);
+	swatch3 = _swatchGenerator(s3, b);
 	printf("%s\n%s\n", color, swatch1);
 	return (0);
 }
 
-unsigned char *_rgbGenerator(unsigned char *c)
+unsigned char *_rgbGenerator(unsigned char *c, int *r, int *g, int *b)
 {
-	sprintf(c, "%02x%02x%02x", rand() % 255,
-		rand() % 255, rand() % 255);
+	sprintf(c, "%d%d%d", *r, *g, *b);
 	return (c);
 }
 
-unsigned char *_swatchGenerator(unsigned char *color, unsigned char *swatch)
+unsigned char *_swatchGenerator(unsigned char *swatch, int *change_val)
 {
-	strcpy(swatch, color);
+	swatch = _rgbGenerator(swatch
 	return (swatch);
 }

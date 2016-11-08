14280837ccc95299c19bbfb1466522fb4947e701
Jennie Chu <jenniechu92@gmail.com>
2016-11-08 04:40:40 +0000

    Generates three different swatches

--- _randomRGB.c
+++ _randomRGB.c
@@ -14,7 +14,7 @@
 
 
 unsigned char *_rgbGenerator(unsigned char *c, int *r, int *g, int *b);
-unsigned char *_swatchGenerator(unsigned char *color, unsigned char *swatch);
+unsigned char *_swatchGenerator(unsigned char *color, int *r, int *g, int *b);
 unsigned int *_randomVal(unsigned int *val);
 
 int main(void)
@@ -32,10 +32,14 @@ int main(void)
 	g = &g_val;
 	b = &b_val;
 	color = _rgbGenerator(c, r, g, b);
-	swatch1 = _swatchGenerator(s1, r);
-	swatch2 = _swatchGenerator(s2, g);
-	swatch3 = _swatchGenerator(s3, b);
-	printf("%s\n%s\n", color, swatch1);
+	strcpy(s1, color);
+	swatch1 = _swatchGenerator(s1, r, g, b);
+	strcpy(s2, color);
+	swatch2 = _swatchGenerator(s2, r, g, b);
+	strcpy(s3, color);
+	swatch3 = _swatchGenerator(s3, r, g, b);
+	printf("Main Color: %s\nFirst Swatch: %s\nSecond Swatch: %s\n"
+	       "Third Swatch: %s\n", color, swatch1, swatch2, swatch3);
 	return (0);
 }
 
@@ -45,8 +49,21 @@ unsigned char *_rgbGenerator(unsigned char *c, int *r, int *g, int *b)
 	return (c);
 }
 
-unsigned char *_swatchGenerator(unsigned char *swatch, int *change_val)
+unsigned char *_swatchGenerator(unsigned char *swatch, int *r, int *g, int *b)
 {
-	swatch = _rgbGenerator(swatch
+	int *temp_r, *temp_g, *temp_b;
+	int r_num;
+
+	temp_r = r;
+	temp_g = g;
+	temp_b = b;
+	r_num = (rand() % 2) + 1;
+	if (r_num == 1)
+		*temp_r = *temp_r +  10;
+	else if (r_num == 2)
+		*temp_g = *temp_g +  10;
+	else
+		*temp_b = *temp_b +  10;
+	sprintf(swatch, "%d%d%d", *temp_r,  *temp_g, *temp_b);
 	return (swatch);
 }

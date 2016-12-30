6a4c4acab34a3871211fe059cbd340c18a41f4cb
Jennie Chu <jenniecu92@gmail.com>
2016-12-30 15:35:52 -0800

    Created Outlets & Set Background Color in viewDidLoad

--- RGBA/RGBA/ViewController.swift
+++ RGBA/RGBA/ViewController.swift
@@ -1,4 +1,5 @@
 //
+
 //  ViewController.swift
 //  RGBA
 //
@@ -9,13 +10,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var ogColor: UILabel!
+    @IBOutlet weak var colorOne: UIButton!
+    @IBOutlet weak var colorTwo: UIButton!
+    @IBOutlet weak var colorThree: UIButton!
+    @IBOutlet weak var scoreCount: UILabel!
+    var guesses = [String]()
+    var score = 0
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // get random RGB values
+        let color1 = 0.5
+        // set colors
+        ogColor.backgroundColor = UIColor(red: CGFloat(color1), green: 0.6, blue: 0.2, alpha: 1.0)
+        colorOne.layer.backgroundColor = UIColor.brown.cgColor
+        colorTwo.layer.backgroundColor = UIColor.cyan.cgColor
+        colorThree.layer.backgroundColor = UIColor.darkGray.cgColor
     }
-
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
